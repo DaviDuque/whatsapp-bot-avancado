3f6d9e120063a375a5b299f21cb6024dabd3a7f0
Davi Duque do nascimento <davi1programador@gmail.com>
2024-11-22 11:30:19 -0300

    tewste

--- src/infra/integrations/twilio.ts
+++ src/infra/integrations/twilio.ts
@@ -105,7 +105,7 @@ export async function sendConfirmPadraoMessage( To: string, From: string, dados:
 }
 
 // Serviço para enviar o arquivo via WhatsApp
-export const sendFileViaWhatsApp = async (To: string, From: string, filename: string) => {
+export const sendFileViaWhatsApp = async ( From: string, To: string, filename: string) => {
   try {
     const fileUrl = `${URL}/file/${filename}`; // URL do arquivo
  console.log("dados...", To, From, fileUrl)
